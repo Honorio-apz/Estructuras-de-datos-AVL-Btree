0999f291bc4ea9f434abe206a8c8e39d2b3c5103
Honorio-apz <72112706+Honorio-apz@users.noreply.github.com>
2021-07-03 06:57:51 -0500

    version oficial

--- AVL-Tree/public/main.js
+++ AVL-Tree/public/main.js
@@ -1,36 +1,37 @@
-let previousKey = false; // Helper variable for finding a node
-let toggleValues = false; // Helper variablefor toggle button
-let maxpath = 0; // Helpers for getting tree/node height
+let previousKey = false; // Variable auxiliar para encontrar un nodo
+let toggleValues = false; // Variable auxiliar para botón de alternancia
+let maxpath = 0; // Ayudantes para obtener la altura del árbol / nodo
 let maxH = 0;
 
 function setup() {
 	canvas = createCanvas(windowWidth, windowHeight);
 	canvas.position(0, 0);
-	canvas.style("z-index", "-1"); // Put canvas under the document
-	background(51);
+	canvas.style("z-index", "-1"); 
+	// Pon un fondo debajo del documento
+	colorMode(HSB);
+	background('red');
 
-	// Fill tree with random nodes
+	// Llenar árbol con nodos aleatorios
 	tree = new Tree();
 	for (let i = 0; i < 15; i++) {
 		try {
 			tree.insertNode(floor(random(0, 100)), "node");
 		} catch (e) {
-			// One in about 100 times there's some weird error
-			// occuring in here. When it happens, quickly reload page
 			location.reload();
 		}
 	}
 }
 
-function draw() { } // Required by p5, but not used here
+function draw() { } // Requerido por p5, pero no usado aquí
 
-// Event handlers
+// Controladores de eventos
 document
 	.getElementsByClassName("onoffswitch-label")[0]
 	.addEventListener("click", e => {
 		toggleValues = !toggleValues;
 		clear();
-		background(51);
+		colorMode(HSB);
+		background(255, 204, 100);
 		tree.preOrder(toggleValues);
 	});
 
@@ -47,7 +48,7 @@ document.getElementsByClassName("insertBtn")[0].addEventListener("click", e => {
 	else { this.mensajeExiste() };
 	tree.insertNode(key, val);
 
-	// Clear input fields
+	// Borrar campos de entrada
 	document.getElementsByClassName("key")[0].value = "";
 	document.getElementsByClassName("value")[0].value = "";
 });
@@ -63,17 +64,18 @@ document.getElementsByClassName("removeBtn")[0].addEventListener("click", e => {
 	if (i < 0) { this.alerta() };
 	tree.removeNode(key);
 
-	// Clear input field
+	// Borrar campo de entrada
 	document.getElementsByClassName("key")[1].value = "";
 });
 
 document.getElementsByClassName("findBtn")[0].addEventListener("click", e => {
 	e.preventDefault();
 
-	// Clear previous highlights
+	// Borrar aspectos destacados anteriores
 	if (previousKey) {
 		clear();
-		background(51);
+		colorMode(HSB);
+		background(255, 204, 100);
 		tree.preOrder();
 	}
 
